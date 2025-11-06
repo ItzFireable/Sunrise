16d8fb51b9394a5d8a8ab161d050e7eaabf78016
ItzFireable <fireableplayz@gmail.com>
2025-11-07 00:19:34 +0200

    feat: Add migrations for pinned scores

---
+++ Sunrise.Shared/Database/Migrations/20251106193321_AddPinnedScores.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sunrise.Shared.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPinnedScores : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_score_BeatmapId_IsScoreable_IsPassed_SubmissionStatus",
+                table: "score");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPinned",
+                table: "score",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_score_BeatmapId_IsScoreable_IsPassed_IsPinned_SubmissionStat~",
+                table: "score",
+                columns: new[] { "BeatmapId", "IsScoreable", "IsPassed", "IsPinned", "SubmissionStatus" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_score_BeatmapId_IsScoreable_IsPassed_IsPinned_SubmissionStat~",
+                table: "score");
+
+            migrationBuilder.DropColumn(
+                name: "IsPinned",
+                table: "score");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_score_BeatmapId_IsScoreable_IsPassed_SubmissionStatus",
+                table: "score",
+                columns: new[] { "BeatmapId", "IsScoreable", "IsPassed", "SubmissionStatus" });
+        }
+    }
+}
